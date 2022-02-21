4d8e61629372698e2ed1ed93a3c39f6fa7bbe190
Chamath Wanigasooriya <chamath.wanigasooriya@the-ai.team>
2022-02-21 18:50:30 +0530

    🐳 chore(routing): add routing

---
+++ src/layouts/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+    BrowserRouter as Router, Link
+} from "react-router-dom";
+import routes from "../routes/routes";
+
+export default function Layout() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                {/*
+            A <Switch> looks through all its children <Route>
+            elements and renders the first one whose path
+            matches the current URL. Use a <Switch> any time
+            you have multiple routes, but you want only one
+            of them to render at a time
+          */}
+                {routes}
+            </div>
+        </Router>
+    );
+}
