5a95dc3b98e1479b7c9624c42645f6fe76eea9c7
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2022-02-24 12:38:04 +0530

    :rainbox: style(layout): text layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,34 +5,107 @@ import CardContent from '@mui/material/CardContent';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MuiGrid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const Input = styled('input')({
     display: 'none',
 });
 
-export default function Home() {
-    return (
-        <Card
-            sx={{
-                width: 900,
-                height: 500,
-                m: 1,
-                flexWrap: 'wrap',
-            }}>
-            <CardContent>
+const Grid = styled(MuiGrid)(({ theme }) => ({
+    width: '100%',
+    ...theme.typography.body2,
+    '& [role="separator"]': {
+        margin: theme.spacing(0, 2),
+    },
+}));
 
-            </CardContent>
-            <CardActions>
-                <label htmlFor="text-button-file">
-                    <Input accept=".doc,.docx,.pdf" id="text-button-file" multiple type="file" />
-                    <Button variant="text" component="span"
-                        style={{
-                            textTransform: 'lowercase',
-                        }}>
-                        Upload a .docx, .pdf
-                    </Button>
-                </label>
-            </CardActions>
-        </Card>
+export default function Home() {
+
+    const [language, setLanguage] = React.useState('');
+
+    const handleChange = (event) => {
+        setLanguage(event.target.value);
+    };
+
+    const content = (
+        <div style={{ paddingTop: "50px", height: "300px"}}>
+            {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id dignissim justo.
+       Nulla ut facilisis ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+       Sed malesuada lobortis pretium.`}
+        </div>
+    );
+
+    return (
+        <div style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate( -50%, -50%)"
+        }} >
+            <Card
+                sx={{
+                    width: 850,
+                    height: 500,
+                    flexWrap: 'wrap',
+                }}>
+
+                <CardContent>
+                    <Grid container>
+                        <Grid item xs>
+                            <p style={{ fontSize: "16px" }}>English</p>
+
+                            {content}
+                        </Grid>
+
+                        <Divider orientation="vertical" flexItem>
+                        </Divider>
+
+                        <Grid item xs>
+                            <Box sx={{ minWidth: 120 }}>
+                                <FormControl fullWidth size="small">
+                                    <InputLabel id="demo-simple-select-label">Translate to</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={language}
+                                        label="Translate"
+                                        onChange={handleChange}
+                                    >
+                                        <MenuItem value={10}>German</MenuItem>
+                                        <MenuItem value={20}>French</MenuItem>
+                                        <MenuItem value={30}>Greek</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                            {content}
+                        </Grid>
+                    </Grid>
+
+                </CardContent>
+                <CardActions>
+                    <label htmlFor="text-button-file">
+                        <Input accept=".doc,.docx,.pdf" id="text-button-file" multiple type="file" />
+                        <Button variant="text" component="span"
+                            style={{
+                                textTransform: 'none',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                flexWrap: 'wrap'
+                            }}>
+                            <CloudUploadIcon />
+                            Upload a .docx, .pdf
+                        </Button>
+                    </label>
+                </CardActions>
+            </Card>
+        </div>
     );
 }
