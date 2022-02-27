8f6dd6511332cacc504da0d037fd89a5a5107ed7
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2022-02-27 09:43:24 +0530

    :tada: feat(flask): init flask backend

--- api/server.py
+++ api/server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World'
+    return 'Hello'
 
 if __name__ == '__main__':
     app.run(debug=True)
